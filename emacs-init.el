dde6a813ced374adbf2f4956690b06f7cc7d6ab6
vitor quintanilha barbosa <vitorqb@gmail.com>
2019-06-21 19:46:35 +0200

    OPT Corrects melpa-stable url

--- emacs-init.el
+++ emacs-init.el
@@ -35,7 +35,7 @@
 ;; -----------------------------------------------------------------------------
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives '("melpa-stable" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 (if (not (package-installed-p 'use-package))
     (progn
