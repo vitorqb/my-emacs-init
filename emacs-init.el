e8a206d3c9905d263f3556a6411527bd694be053
vitor quintanilha barbosa <vitorqb@gmail.com>
2019-07-23 18:22:53 +0200

    FIX Don't customize anything!

--- emacs-init.el
+++ emacs-init.el
@@ -48,6 +48,9 @@
 ;; -----------------------------------------------------------------------------
 ;; Packages and load settings
 ;; -----------------------------------------------------------------------------
+;; Don't customize anything
+(setq custom-file "/dev/null")
+
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
