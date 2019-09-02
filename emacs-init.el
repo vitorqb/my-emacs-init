b4c23a879751d5f5c50d179c0d0244ca1fd87d6e
Vitor Barbosa <vitor.barbosa@greenpowermonitor.com>
2019-09-02 08:52:43 +0200

    NEW Adds org repo

--- emacs-init.el
+++ emacs-init.el
@@ -54,6 +54,8 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+
 (package-initialize)
 (if (not (package-installed-p 'use-package))
     (progn
