3fc5028f214d43f4171365e065b25ac7a9fe67e8
vitor quintanilha barbosa <vitorqb@gmail.com>
2019-09-02 19:16:03 +0200

    NEW Adds `copy-line`

--- emacs-init.el
+++ emacs-init.el
@@ -247,15 +247,38 @@
 (global-set-key (kbd "C-~") 'delete-trailing-whitespace)
 (global-set-key (kbd "<f11>") (myutils/li (insert (projectile-project-root))))
 
+;; Copied from emacs wiki
+(defun copy-line (arg)
+  "Copy lines (as many as prefix argument) in the kill ring.
+      Ease of use features:
+      - Move to start of next line.
+      - Appends the copy on sequential calls.
+      - Use newline as last char even on the last line of the buffer.
+      - If region is active, copy its lines."
+  (interactive "p")
+  (let ((beg (line-beginning-position))
+        (end (line-end-position arg)))
+    (when mark-active
+      (if (> (point) (mark))
+          (setq beg (save-excursion (goto-char (mark)) (line-beginning-position)))
+        (setq end (save-excursion (goto-char (mark)) (line-end-position)))))
+    (if (eq last-command 'copy-line)
+        (kill-append (buffer-substring beg end) (< end beg))
+      (kill-ring-save beg end)))
+  (kill-append "\n" nil)
+  (beginning-of-line (or (and arg (1+ arg)) 2))
+  (if (and arg (not (= 1 arg))) (message "%d lines copied" arg)))
+
 (defun my/setup-hydra/typing-hydra ()
   "Prepares an hydra for typing shortcuts."
   (defhydra my/typing-hydra (:color blue)
     "An hydra for typing shortcuts!\n"
-    ("f" #'myutils/fill-to-end "Fill to end with '-'.\n")
-    ("d" #'myutils/insert-date "Insert the date.\n")
-    ("e" #'myutils/remove-with-elipsis "Remove with elipsis.\n" :color pink)
     ("c" #'myutils/remove-whitespace-and-newline
      "Clean - remove whitesapces and newlines.\n")
+    ("d" #'myutils/insert-date "Insert the date.\n")
+    ("e" #'myutils/remove-with-elipsis "Remove with elipsis.\n" :color pink)
+    ("f" #'myutils/fill-to-end "Fill to end with '-'.\n")
+    ("l" #'copy-line "Copies current line down.\n")
     ("w" #'delete-trailing-whitespace "Delete trailing whitespaces.\n")))
 
 ;; -----------------------------------------------------------------------------
