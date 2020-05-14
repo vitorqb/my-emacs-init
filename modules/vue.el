aa6b400b13649614e2a8abe07d282e166277bb16
vitor quintanilha barbosa <vitorqb@gmail.com>
2020-05-14 19:10:26 +0200

    New Minimal vue setup

--- modules/vue.el
+++ modules/vue.el
@@ -1,1 +1,12 @@
-(use-package vue-mode :ensure)
+;; Use dev version for mmm-mode - REQUIRES MANUAL STEPS.
+;; See https://github.com/AdamNiederer/vue-mode/issues/99
+;; https://github.com/purcell/mmm-mode/issues/99
+(use-package mmm-mode
+  :ensure
+  :load-path "/home/vitor/git-others/mmm-mode")
+
+(use-package vue-mode
+  :ensure
+  :config (progn
+            (setq mmm-js-mode-enter-hook (lambda () (setq syntax-ppss-table nil)))
+            (setq mmm-typescript-mode-enter-hook (lambda () (setq syntax-ppss-table nil)))))
