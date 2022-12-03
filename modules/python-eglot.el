07a104966dfe884b56abd2e6a24dd8df7675d30d
Vitor Barbosa <vitorqb@gmail.com>
2022-12-03 14:42:34 +0100

    Adds pyenv to python eglot

--- modules/python-eglot.el
+++ modules/python-eglot.el
@@ -4,5 +4,8 @@
 ;; For jinaj2 template engine
 (use-package jinja2-mode :ensure)
 
+;; For virtual environments
+(use-package pyvenv :ensure)
+
 ;; Snippets S2
 (add-hook 'python-mode-hook #'yas-minor-mode-on)
