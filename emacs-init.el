930bea485d08b9902a3fb46e5a37e5d6d7d804ed
Vitor Barbosa <vitorqb@gmail.com>
2023-06-21 20:06:11 +0200

    Removes deprecated org repo

--- emacs-init.el
+++ emacs-init.el
@@ -59,7 +59,7 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+;; (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
 
 (package-initialize)
 (if (not (package-installed-p 'use-package))
