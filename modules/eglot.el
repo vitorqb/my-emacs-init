46f126598428ba6fc32ce6fc8cce8e3b4fcafb0f
Vitor Barbosa <vitorqb@gmail.com>
2023-09-22 12:56:32 +0200

    Add eglot hydra if to main hydra

--- modules/eglot.el
+++ modules/eglot.el
@@ -1,9 +1,19 @@
 ;; -----------------------------------------------------------------------------
 ;; Language Server Protocol
 ;; -----------------------------------------------------------------------------
+(defun my/setup-hydra/eglot-hydra ()
+  "Prepares an hydra for eglot."
+  (defhydra my/eglot-hydra (:color blue)
+    ("e" #'eglot "Start/restart" :column "Eglot")
+    ("a" #'eglot-code-actions "Eglot code actions")
+    ("r" #'eglot-rename "Eglot rename")))
+
 (use-package eglot :ensure
   :config (progn
             ;; If we are using eglot, bind M-. to xref, that eglot itself uses
             (add-hook
              'eglot--managed-mode-hook
-             (lambda () (local-set-key (kbd "M-.") 'xref-find-definitions)))))
+             (lambda () (local-set-key (kbd "M-.") 'xref-find-definitions)))
+
+            ;; Refresh hydra
+            (my/hydras-setup)))
