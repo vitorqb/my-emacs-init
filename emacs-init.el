947091d4696824a9782e032d6f9bc68fc6135cf2
Vitor Barbosa <vitorqb@gmail.com>
2024-09-16 09:49:27 +0200

    feat: add delete-file-and-buffer function

--- emacs-init.el
+++ emacs-init.el
@@ -343,6 +343,19 @@
 (global-set-key (kbd "C-c d") #'myutils/duplicate-buffer)
 (global-set-key (kbd "C-x C-g") #'push-mark-and-avy-goto-char)
 
+;; based on http://emacsredux.com/blog/2013/04/03/delete-file-and-buffer/
+(defun delete-file-and-buffer ()
+  "Kill the current buffer and deletes the file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (if filename
+        (if (y-or-n-p (concat "Do you really want to delete file " filename " ?"))
+            (progn
+              (delete-file filename)
+              (message "Deleted file %s." filename)
+              (kill-buffer)))
+      (message "Not a file visiting buffer!"))))
+
 (defun my/highligh-region (beg end)
   "Highlights text equal to the text between beg and end"
   (interactive "r")
