110cc1c3719c1fa7a5b37fc6e66e3a19f3e2ae70
Vitor Barbosa <vitorqb@gmail.com>
2025-03-09 16:55:20 +0100

    feat: update doom emacs config and theme

--- emacs-init.el
+++ emacs-init.el
@@ -172,12 +172,10 @@
     ;; Global settings (defaults)
     (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
           doom-themes-enable-italic t) ; if nil, italics is universally disabled
-    (load-theme 'doom-one t)
+    (load-theme 'doom-acario-dark t)
 
     ;; Enable flashing mode-line on errors
     (doom-themes-visual-bell-config)
-    ;; Enable custom neotree theme (nerd-icons must be installed!)
-    (doom-themes-neotree-config)
     ;; or for treemacs users
     (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
     (doom-themes-treemacs-config)
