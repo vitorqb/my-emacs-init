c02fe4eeb97df8c51e7e4cb0caaf5f19a0efb357
Vitor Barbosa <vitorqb@gmail.com>
2025-03-10 19:59:43 +0100

    Update theme

--- emacs-init.el
+++ emacs-init.el
@@ -172,13 +172,11 @@
     ;; Global settings (defaults)
     (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
           doom-themes-enable-italic t) ; if nil, italics is universally disabled
-    (load-theme 'doom-acario-dark t)
+    (load-theme 'doom-tomorrow-night t)
 
     ;; Enable flashing mode-line on errors
     (doom-themes-visual-bell-config)
-    ;; or for treemacs users
-    (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
-    (doom-themes-treemacs-config)
+    
     ;; Corrects (and improves) org-mode's native fontification.
     (doom-themes-org-config)))
 
