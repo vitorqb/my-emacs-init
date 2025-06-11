e53083a746d18defb11e63277e47476b0081ecf0
Vitor Barbosa <vitorqb@gmail.com>
2025-06-11 20:07:50 +0200

    feat(deadgrep): nice grep-find with `fd` and `rg`

--- modules/deadgrep.el
+++ modules/deadgrep.el
@@ -13,9 +13,24 @@
   (let ((deadgrep-project-root-function (lambda () default-directory)))
     (call-interactively #'deadgrep )))
 
+;; Customize grep-find to use `rg`
+;; Copied from `grep.el`
+;; Notice it also requires `fd`: https://github.com/sharkdp/fd
+(setq deadgrep-find-command
+      '("fd . --type f --exec rg --no-heading --color=auto -nH --null -e  \\{\\}" . 65))
+(defun my/deadgrep/grep-find (command-args)
+  (interactive
+   (list (read-shell-command "Run find (like this): " deadgrep-find-command 'grep-find-history)))
+  (grep command-args))
+
 ;; Custom hydra
 (defhydra my/deadgrep-hydra (:color blue)
   ("a" #'my/deadgrep-current-dir "Simple search on default dir" :column "Deadgrep!")
-  ("p" #'my/deadgrep-project-root "Search on project root"))
+  ("p" #'my/deadgrep-project-root "Search on project root")
+  ("f" #'my/deadgrep/grep-find "grep-find on current dir")
+  ("F" (lambda () (interactive)
+         (projectile-with-default-dir (projectile-acquire-root)
+           (call-interactively #'my/deadgrep/grep-find)))
+   "grep-find on project root"))
 
 (my/hydras-setup)
