2908d199bdea0896a1411a7e6f3ef605d18bc438
Vitor Barbosa <vitorqb@gmail.com>
2025-07-02 15:10:35 +0200

    refact: update python-eglot mode

--- modules/python-eglot.el
+++ modules/python-eglot.el
@@ -4,17 +4,12 @@
 ;; For jinaj2 template engine
 (use-package jinja2-mode :ensure)
 
-;; For virtual environments
-(use-package pyvenv :ensure)
-
 ;; Snippets S2
 (add-hook 'python-mode-hook #'yas-minor-mode-on)
 
-;; Variable with start to start pyright
-(defvar my/python-eglot/pyright-server (expand-file-name "~/tools/pyright/run.sh"))
+;; How to run `pyright`
+(defvar my/python-eglot/pyright-server
+  `("mise" "exec" "--" ,(expand-file-name "~/tools/pyright/run.sh")))
 
-;; Help for activating virtual environment
-(defun my/python-eglot/start-pyright-langserver (arg)
-  (list my/python-eglot/pyright-server))
-
-(add-to-list 'eglot-server-programs `(python-mode . ,#'my/python-eglot/start-pyright-langserver))
+;; Tells eglot how to run pyrgith
+(add-to-list 'eglot-server-programs `(python-mode . ,my/python-eglot/pyright-server))
