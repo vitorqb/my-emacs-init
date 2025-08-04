d2262f9c8b58206225d4f9e97bd479486c5f4617
Vitor Barbosa <vitorqb@gmail.com>
2025-08-04 10:45:05 +0200

    feat(magic-ext): add initial magit extension package with base setup
    - Add new package magic-ext with Emacs Lisp source and empty test scaffold
    - Include Cask config for package dependencies (dash, s, magit)
    - Add Makefile for install, compile, and test automation with cask and Emacs
    - Add .gitignore entry for .cask directory and empty .projectile file
    - Implement magit-ext.el with commit message generation helper functions that wrap git diff and AI chat prompt integration

---
+++ packages/magic-ext/magit-ext-test.el
@@ -0,0 +1,18 @@
+;;; magit-ext-test.el --- Magit extensions -*- lexical-binding: t -*-
+
+;; Copyright (C) 2010-2025 Vitor Quintanilha Barbosa
+
+;; Author: Vitor <vitorqb@gmail.com>
+;; Version: 0.0.1
+;; Maintainer: Vitor <vitorqb@gmail.com>
+;; Created: 2025-08-04
+;; Keywords: elisp
+;; Homepage: https://github.com/vitorqb/my-emacs-init
+
+;; This file is not part of GNU Emacs.
+
+;;; code
+
+
+
+;;; magit-ext-test.el ends here
---
+++ packages/magic-ext/magit-ext.el
@@ -0,0 +1,48 @@
+;;; magit-ext.el --- Magit extensions -*- lexical-binding: t -*-
+
+;; Copyright (C) 2010-2025 Vitor Quintanilha Barbosa
+
+;; Author: Vitor <vitorqb@gmail.com>
+;; Version: 0.0.1
+;; Maintainer: Vitor <vitorqb@gmail.com>
+;; Created: 2025-08-04
+;; Keywords: elisp
+;; Homepage: https://github.com/vitorqb/my-emacs-init
+
+;; This file is not part of GNU Emacs.
+
+;;; code
+(provide 'magit-ext)
+
+(setq magext--aichat-prompt "You are an experienced developer. Analyze carefully the git diff below. Create a concize and clear commit message based on the changes. Follow all best-practices for commit messages. Follow the \"Semantic Commits\" standard.")
+
+(defun magext--staged-changes-to-file (dir)
+  "Saves the staged changes to a file. Returns the file."
+  (let ((tmpfile (make-temp-file "magext--staged-changes-to-file"))
+        (git-diff-cmd (format "git -C '%s' diff --staged" dir)))
+    (with-temp-buffer
+      (call-process-region
+       nil                  ; START=nil -> send entire buffer contents
+       nil                  ; END=nil   -> ignored
+       "git"                ; PROGRAM
+       nil                  ; DELETE=nil -> Don't delete after sending
+       `(:file ,tmpfile)    ; BUFFER -> Save result to tmpfile
+       nil                  ; DISPLAY -> Don't display
+       "-C" dir "diff" "--staged"       ;ARGS for PROGRAM
+       ))
+    tmpfile))
+
+(defun magext--commit-msg (diff-file)
+  "Generates a commit msg for a git diff output"
+  (shell-command-to-string (format "aichat -f '%s' '%s'" diff-file magext--aichat-prompt)))
+
+(defun magext-prefill-commit-msg (dir)
+  "Prefills the commit msg magit buffer with a generated one based on your git changes"
+  (interactive (list default-directory))
+  (let* ((default-directory dir)
+         (diff-file (magext--staged-changes-to-file dir))
+         (commit-msg (magext--commit-msg diff-file)))
+    (goto-char (point-min))
+    (insert commit-msg)))
+
+;;; magit-ext.el ends here
