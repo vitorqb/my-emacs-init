29c2bbb12a52b08b78fe1443b0649ce6a7530dbe
Vitor Barbosa <vitorqb@gmail.com>
2025-08-12 09:37:11 +0200

    fix(elixir): fixes elixir setup

--- modules/elixir.el
+++ modules/elixir.el
@@ -1,3 +1,6 @@
+;; You will need to install the LSP somehow.
+;; Follow https://github.com/elixir-lsp/elixir-ls#detailed-installation-instructions
+
 (emacs-init-load-module-eglot)
 (use-package elixir-mode :ensure
   ;; Set eglot-connect-timeout to 120 (default is 30) because the elixir lsp
@@ -5,18 +8,6 @@
   :hook ((elixir-mode . (lambda () (setq eglot-connect-timeout 120)))))
 
 (defvar emacs-init-elixir/lsp-program
-  (expand-file-name "~/tools/elixir-ls/language_server.sh"))
+  `("mise" "exec" "--" ,(expand-file-name "~/tools/elixir-ls/language_server.sh")))
 
-;; You will need to install the LSP somehow.
-;; Follow https://github.com/elixir-lsp/elixir-ls#detailed-installation-instructions
-(when (not (file-exists-p emacs-init-elixir/lsp-program))
-  (display-warning
-   :error
-   (s-concat "Could not fund executable for elixir-ls "
-             "(language server) in "
-             emacs-init-elixir/lsp-program
-             ". Maybe you need to install it?"
-             " Check "
-             "https://github.com/elixir-lsp/elixir-ls#detailed-installation-instructions")))
-
-(add-to-list 'eglot-server-programs `(elixir-mode ,emacs-init-elixir/lsp-program))
+(add-to-list 'eglot-server-programs `(elixir-mode . ,emacs-init-elixir/lsp-program))
