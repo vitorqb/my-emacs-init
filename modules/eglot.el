c515c5337f0e9fad7f958769407dbaa9afaff6dd
Vitor Barbosa <vitorqb@gmail.com>
2025-08-27 10:49:02 +0200

    feat(eglot): add inline hints toggle and adjust hydra menu layout
    - Reordered eglot hydra menu entries for improved usability
    - Added "h" key binding to toggle eglot-inlay-hints-mode
    - Configured eglot to report progress messages to *Message* buffer for better visibility

--- modules/eglot.el
+++ modules/eglot.el
@@ -4,11 +4,13 @@
 (defun my/setup-hydra/eglot-hydra ()
   "Prepares an hydra for eglot."
   (defhydra my/eglot-hydra (:color blue)
-    ("e" #'eglot "Start/restart" :column "Eglot")
-    ("a" #'eglot-code-actions "Eglot code actions")
+    ("a" #'eglot-code-actions "Eglot code actions" :column "Eglot")
+    ("e" #'eglot "Start/restart")
+    ("h" #'eglot-inlay-hints-mode "Inline Hints mode")
     ("r" #'eglot-rename "Eglot rename")))
 
 (use-package eglot :ensure
+  :custom (eglot-report-progress 'messages "Send eglot progress messages to *Message*")
   :config (progn
             ;; If we are using eglot, bind M-. to xref, that eglot itself uses
             (add-hook
