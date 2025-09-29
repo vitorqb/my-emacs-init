d2892cdd937660a725c2988c15de075b1c08ccfa
Vitor Barbosa <vitorqb@gmail.com>
2025-09-29 15:13:21 +0200

    feat(my-gh): add browse URL copy to browse commands
    - Introduce `my/gh/browse-url-to-clipboard` command to copy GitHub browse URL to clipboard using `gh browse --no-browser`
    - Add customizable `my/gh/copy-to-clipboard` function for clipboard interaction, defaulting to `kill-new`
    - Extend hydra menu with "B" binding to invoke URL copy command

--- packages/my-gh/my-gh-test.el
+++ packages/my-gh/my-gh-test.el
@@ -4,11 +4,23 @@
 
 (ert-deftest my/gh//browse-cmd ()
   (should (equal (my/gh//browse-cmd "foo" nil) "gh browse foo"))
-  (should (equal (my/gh//browse-cmd "foo" 12)  "gh browse foo:12")))
+  (should (equal (my/gh//browse-cmd "foo" 12)  "gh browse foo:12"))
+  (should (equal (my/gh//browse-cmd "foo" 12 't)  "gh browse --no-browser foo:12")))
 
 (ert-deftest my/gh//browse-commit-cmd ()
   (should (equal (my/gh//browse-commit-cmd "7fa72cc") "gh browse 7fa72cc"))
-  (should (equal (my/gh//browse-commit-cmd '7fa72cc) "gh browse 7fa72cc")))
+  (should (equal (my/gh//browse-commit-cmd '7fa72cc) "gh browse 7fa72cc"))
+  (should (equal (my/gh//browse-commit-cmd '7fa72cc 't) "gh browse --no-browser 7fa72cc")))
+
+(ert-deftest my/gh/browse-url-to-clipboard ()
+  (let* ((fake-clipboard "")
+         (my/gh/copy-to-clipboard (lambda (x) (setq fake-clipboard x))))
+    (cl-letf (((symbol-function 'shell-command-to-string)
+               (lambda (x)
+                 (should (equal x "gh browse --no-browser foo:1"))
+                 "http:://foo.com#1")))
+      (my/gh/browse-url-to-clipboard "foo" 1)
+      (should (equal fake-clipboard "http:://foo.com#1")))))
 
 
 ;;; my-gh-test.el ends here
--- packages/my-gh/my-gh.el
+++ packages/my-gh/my-gh.el
@@ -21,6 +21,9 @@
     (shell-command "i3-msg [urgent=latest] focus"))
   "Callback to be run when we send something to the browser")
 
+(defvar my/gh/copy-to-clipboard
+  (lambda (x) (kill-new x)))
+
 (defun my/gh/open-repo-on-browser ()
   (interactive)
   (shell-command "gh repo view --web")
@@ -52,12 +55,14 @@
                (fboundp 'markdown-mode))
       (markdown-mode))))
 
-(defun my/gh//browse-cmd (file-path &optional line-number)
+(defun my/gh//browse-cmd (file-path &optional line-number no-browser)
   "Returns the `gh` command to be used for browsing to a file/line number"
   (let* ((default-directory (or (file-name-directory file-path) default-directory))
          (file-name (file-name-nondirectory file-path))
          (file-ref (if line-number (format "%s:%s" file-name line-number) file-name)))
-    (format "gh browse %s" file-ref)))
+    (if no-browser
+        (format "gh browse --no-browser %s" file-ref)
+      (format "gh browse %s" file-ref))))
 
 (defun my/gh/browse (file-path &optional line-number)
   "Browses to the current file/line."
@@ -65,14 +70,24 @@
   (shell-command (my/gh//browse-cmd file-path line-number))
   (funcall my/gh/on-browser-open-request))
 
+(defun my/gh/browse-url-to-clipboard (file-path &optional line-number)
+  "Copies the browse URL to clipboard"
+  (interactive (list buffer-file-name (line-number-at-pos)))
+  (->> (my/gh//browse-cmd file-path line-number 't)
+       (shell-command-to-string)
+       (s-trim)
+       (funcall my/gh/copy-to-clipboard)))
+
 (defun my/gh/atlantis-plan ()
   (interactive)
   (let ((cmd-out (shell-command-to-string "gh pr comment --body='atlantis plan'")))
     (browse-url (string-trim cmd-out))))
 
-(defun my/gh//browse-commit-cmd (commit-hash)
+(defun my/gh//browse-commit-cmd (commit-hash &optional no-browser)
   "Returns the `gh` command to be used for browsing to a commit"
-  (format "gh browse %s" commit-hash))
+  (if no-browser
+      (format "gh browse --no-browser %s" commit-hash)
+    (format "gh browse %s" commit-hash)))
 
 (defun my/gh/browse-commit (commit)
   (interactive (list (thing-at-point 'symbol)))
@@ -83,6 +98,7 @@
   (defun my/setup-hydra/gh-hydra ()
     (defhydra my/gh-hydra (:color blue)
       ("b" #'my/gh/browse "Browses to file in github" :column "Github CLI!")
+      ("B" #'my/gh/browse-url-to-clipboard "Copies github browse URL to clipboard")
       ("r" #'my/gh/open-repo-on-browser "Open repo on browser")
       ("p" #'my/gh/open-pr-on-browser "Open PR on browser")
       ("P" #'my/gh/new-pr "Creates a new PR")
