6085d191bb4ab81c7b92c7a2ed166806ca51a619
Vitor Barbosa <vitorqb@gmail.com>
2026-01-24 13:12:13 +0100

    feat(aitool): improves functionalities of AI Tools

--- packages/my-ai-tools/my-ai-tools-test.el
+++ packages/my-ai-tools/my-ai-tools-test.el
@@ -2,7 +2,68 @@
 (require 'my-ai-tools)
 (require 's)
 
-(ert-deftest test/hello-world ()
-  (should (equal 1 1)))
+(ert-deftest test/my/ai-tools/capture-context ()
+
+  ;; No region
+  (with-temp-buffer
+    (insert "Foo\nBar\nBaz")
+    (goto-char 10)
+    (let ((temp-buff (current-buffer))
+          (context   (my/ai-tools/capture-context)))
+      (should (equal 10 (my/ai-tools/context-char-position context)))
+      (should (equal temp-buff (my/ai-tools/context-buffer context)))
+      (should (equal nil (my/ai-tools/context-region-p context)))
+      (should (equal nil (my/ai-tools/context-region-begin context)))
+      (should (equal nil (my/ai-tools/context-region-end context)))))
+
+  ;; Region
+  (with-temp-buffer
+    (insert "Foo\nBar\nBaz")
+    (goto-char 5)
+    (push-mark 10 t t)
+    (activate-mark)
+    (let ((temp-buff (current-buffer))
+          (context   (my/ai-tools/capture-context)))
+      (should (equal 5 (my/ai-tools/context-char-position context)))
+      (should (equal temp-buff (my/ai-tools/context-buffer context)))
+      (should (equal t (my/ai-tools/context-region-p context)))
+      (should (equal 5 (my/ai-tools/context-region-begin context)))
+      (should (equal 10 (my/ai-tools/context-region-end context))))))
+
+(ert-deftest test/my/ai-tools/context-to-string ()
+
+  ;; No region
+  (let* ((tmpfile (make-temp-file "test_my_ai-tools"))
+         (tmpbuff (find-file-noselect tmpfile))
+         (context (make-my/ai-tools/context :buffer tmpbuff
+                                            :region-p nil
+                                            :region-begin nil
+                                            :region-end nil
+                                            :char-position 2)))
+    (with-current-buffer tmpbuff
+      (insert "Foo\nBar"))
+    (should (equal (my/ai-tools/context-to-string context)
+                   (format "## Context\n\nFile: @%s\n\nActive line: 5\n" tmpfile)))
+    (delete-file tmpfile)
+    (set-buffer-modified-p nil)
+    (kill-buffer tmpbuff))
+
+  ;; Region
+  (let* ((tmpfile (make-temp-file "test_my_ai-tools"))
+         (tmpbuff (find-file-noselect tmpfile))
+         (context (make-my/ai-tools/context :buffer tmpbuff
+                                            :region-p t
+                                            :region-begin 1
+                                            :region-end 3
+                                            :char-position 2)))
+    (with-current-buffer tmpbuff
+      (insert "Foo\nBar"))
+    (should (equal (my/ai-tools/context-to-string context)
+                   (format "## Context\n\nFile: @%s\n\nSelected Region (lines 1 to 1):\n```\nFo\n```"
+                           tmpfile)))
+    (delete-file tmpfile)
+    (set-buffer-modified-p nil)
+    (kill-buffer tmpbuff)))
+
 ;;; my-mise-test.el ends here
 
--- packages/my-ai-tools/my-ai-tools.el
+++ packages/my-ai-tools/my-ai-tools.el
@@ -14,31 +14,56 @@
 ;;; code
 (provide 'my-ai-tools)
 (require 'markdown-mode)
+(require 'cl-lib)
 
 (defvar my/ai-tools/prompt-builder-buffer-name "*ai-tools/prompt-builder*")
 
-(defun my/ai-tools/generate-context (region-p region-begin region-end orig-buff)
-  (with-temp-buffer
-    (insert "## Context\n\n")
-    (insert (format "File: @%s\n\n" (buffer-file-name orig-buff)))
-    (when region-p
-      (insert (format "Selected Region (lines %s to %s):\n```\n%s\n```"
-                      (with-current-buffer orig-buff
-                        (line-number-at-pos region-begin))
-                      (with-current-buffer orig-buff
-                        (line-number-at-pos region-end))
-                      (with-current-buffer orig-buff
-                        (buffer-substring-no-properties region-begin region-end)))))
-    (buffer-string)))
+;; Represents the *context* the user is when they want to write an AI prompt
+(cl-defstruct my/ai-tools/context
+  buffer
+  region-p
+  region-begin
+  region-end
+  char-position)
 
-(defun my/ai-tools/prompt-from-context (buff region-p region-begin region-end)
+(defun my/ai-tools/capture-context ()
+  "Captures the current user context"
+  (make-my/ai-tools/context
+   :buffer        (current-buffer)
+   :region-p      (region-active-p)
+   :region-begin  (and (region-active-p) (region-beginning))
+   :region-end    (and (region-active-p) (region-end))
+   :char-position (point)))
+
+(defun my/ai-tools/context-to-string (context)
+  "Generates a string describing the user context (in markdown)"
+  (let ((buffer        (my/ai-tools/context-buffer context))
+        (region-p      (my/ai-tools/context-region-p context))
+        (region-begin  (my/ai-tools/context-region-begin context))
+        (region-end    (my/ai-tools/context-region-end context))
+        (char-position (my/ai-tools/context-region-end context)))
+    (with-temp-buffer
+      (insert "## Context\n\n")
+      (insert (format "File: @%s\n\n" (buffer-file-name buffer)))
+      (if region-p
+          (insert (format "Selected Region (lines %s to %s):\n```\n%s\n```"
+                          (with-current-buffer buffer
+                            (line-number-at-pos region-begin))
+                          (with-current-buffer buffer
+                            (line-number-at-pos region-end))
+                          (with-current-buffer buffer
+                            (buffer-substring-no-properties region-begin region-end))))
+        (insert (format "Active line: %s\n" (line-number-at-pos char-position))))
+      (buffer-string))))
+
+(defun my/ai-tools/prompt-from-context (context)
   "Interactively constructs an AI prompt from the given user context"
-  (interactive (list (current-buffer) (region-active-p) (region-beginning) (region-end)))
+  (interactive (list (my/ai-tools/capture-context)))
   (my/ai-tools/maybe-kill-builder-buff)
   (let ((prompt-builder-buf (my/ai-tools/get-or-create-builder-buff)))
     (switch-to-buffer prompt-builder-buf)
     (my/ai-tools/prompt-builder-mode)
-    (insert (my/ai-tools/generate-context region-p region-begin region-end buff))
+    (insert (my/ai-tools/context-to-string context))
     (goto-char (point-min))
     (insert "## User Prompt\n\n\n\n")
     (line-move -2)))
@@ -58,9 +83,9 @@
 
 (defvar my/ai-tools/prompt-builder-mode-map
   (let ((map (make-sparse-keymap)))
-    (set-keymap-parent map markdown-mode-map)
     (define-key map (kbd "C-c C-c") #'my/ai-tools/prompt-builder-done)
     (define-key map (kbd "C-c C-k") #'my/ai-tools/prompt-builder-drop)
+    (set-keymap-parent map markdown-mode-map)
     map))
 
 (define-derived-mode my/ai-tools/prompt-builder-mode markdown-mode "My/AiTools/PromptBuilder"
