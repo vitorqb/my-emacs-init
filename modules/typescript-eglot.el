90d512c816815690636914082ecddf68b7f5331a
Vitor Barbosa <vitorqb@gmail.com>
2026-04-16 22:39:02 +0200

    feat: use mise for typescript eglot

--- modules/typescript-eglot.el
+++ modules/typescript-eglot.el
@@ -6,4 +6,6 @@
   :hook
   ((typescript-mode . yas-minor-mode-on))
   :config
-  (setq typescript-indent-level 2))
+  (setq typescript-indent-level 2)
+  :init
+  (add-to-list 'eglot-server-programs `(typescript-mode . ("mise" "exec" "node@latest" "npm:typescript-language-server@latest" "--" "typescript-language-server" "--stdio"))))
