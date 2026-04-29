5b6b82c9f24c929c19866000ce5f650e38966e61
Vitor Barbosa <vitorqb@gmail.com>
2026-04-29 19:00:05 +0200

    feat(my-ai): Add dired integration

--- packages/my-ai-tools/my-ai-tools-test.el
+++ packages/my-ai-tools/my-ai-tools-test.el
@@ -7,11 +7,15 @@
   ;; No region
   (with-temp-buffer
     (insert "Foo\nBar\nBaz")
+    (cd "/tmp")
+    (setq-local major-mode 'foo)
     (goto-char 10)
     (let ((temp-buff (current-buffer))
           (context   (my/ai-tools/capture-context)))
       (should (equal 10 (my/ai-tools/context-char-position context)))
       (should (equal temp-buff (my/ai-tools/context-buffer context)))
+      (should (equal "/tmp/" (my/ai-tools/context-default-directory context)))
+      (should (equal 'foo (my/ai-tools/context-major-mode context)))
       (should (equal nil (my/ai-tools/context-region-p context)))
       (should (equal nil (my/ai-tools/context-region-begin context)))
       (should (equal nil (my/ai-tools/context-region-end context)))))
@@ -30,7 +34,7 @@
       (should (equal 5 (my/ai-tools/context-region-begin context)))
       (should (equal 10 (my/ai-tools/context-region-end context))))))
 
-(ert-deftest test/my/ai-tools/context-to-string ()
+(ert-deftest test/my/ai-tools/context-to-string/default ()
 
   ;; No region
   (let* ((tmpfile (make-temp-file "test_my_ai-tools"))
@@ -64,6 +68,26 @@
     (delete-file tmpfile)
     (set-buffer-modified-p nil)
     (kill-buffer tmpbuff)))
+
+(ert-deftest test/my/ai-tools/context-to-string/dired ()
+
+  ;; With marked files
+  (with-dired-marked-files '("foo" "bar")
+    (with-temp-buffer
+      (let* ((context (make-my/ai-tools/context :buffer (current-buffer)
+                                                :region-p nil
+                                                :region-begin nil
+                                                :region-end nil
+                                                :char-position 2
+                                                :major-mode 'dired-mode
+                                                :default-directory "/tmp/foo/")))
+        (should (equal (my/ai-tools/context-to-string context)
+                       "## Context\n\nActive Directory: /tmp/foo/\n\nMarked Files:\n  - foo\n  - bar\n"))))))
+
+(defmacro with-dired-marked-files (list-of-files &rest body)
+  (declare (indent 1))
+  `(cl-letf (((symbol-function 'dired-get-marked-files) (lambda (_ _) ,list-of-files)))
+     (progn ,@body)))
 
 ;;; my-mise-test.el ends here
 
